852bf26ce086e705050d1256f39b8649631ea61c
Konstantin Klementiev <konstantin.klementiev@gmail.com>
2017-08-04 23:09:52 +0200

    tweaks to make rtfd.org work

--- doc/conf.py
+++ doc/conf.py
@@ -16,7 +16,8 @@ import os
 sys.path.append('..')
 #sys.path.append(os.path.join('..', 'xrtQook'))
 sys.path.append(os.path.abspath('exts'))
-autodoc_mock_imports = ["PyQt4", "PyQt5", "matplotlib"]
+autodoc_mock_imports = ["PyQt4", "PyQt5", "matplotlib", "matplotlib.ticker",
+                        "matplotlib.pyplot"]
 
 # -- General configuration ----------------------------------------------------
 
