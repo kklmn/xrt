d9d9429c9dbd34704a84cc651c60395d10c34fbb
Konstantin Klementiev <konstantin.klementiev@gmail.com>
2017-08-05 11:33:19 +0200

    tweaks to make rtfd.org work

--- doc/conf.py
+++ doc/conf.py
@@ -12,9 +12,8 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
-sys.path.append('..')
-sys.path.append(os.path.join('..', 'tests', 'raycing'))
+sys.path.insert(0, '..')
+#sys.path.append('..')
 sys.path.append(os.path.abspath('exts'))
 #autodoc_mock_imports = ["PyQt5.QtWebKitWidgets"]
 import matplotlib as mpl
