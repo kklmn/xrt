f2e9bd771eb8ec3be9c85dbc0620918ea6c35994
kklmn <konstantin.klementiev@gmail.com>
2017-10-27 15:37:40 +0200

    reorganized docstrings

--- xrt/gui/xrtQook/__init__.py
+++ xrt/gui/xrtQook/__init__.py
@@ -1,79 +1,34 @@
 # -*- coding: utf-8 -*-
 u"""
-Using xrtQook for script generation
------------------------------------
+The main interface to xrt is through a python script. Many examples of such
+scripts can be found in the supplied folder ‘examples’. The script imports the
+modules of xrt, instantiates beamline parts, such as synchrotron or geometric
+sources, various optical elements, apertures and screens, specifies required
+materials for reflection, refraction or diffraction, defines plots and sets job
+parameters.
 
-- Start xrtQook: type ``python xrtQook.pyw`` from xrt/xrtQook or, if you have
-  installed xrt by running setup.py, type ``xrtQook.pyw`` from any location.
-- Rename beamLine to myTestBeamline by double clicking on it (you do not have
-  to, only for demonstration).
-- Right-click on myTestBeamline and Add Source → BendingMagnet.
+The Qt tool :mod:`xrtQook` takes these ingredients and prepares a ready to use
+script that can be run within the tool itself or in an external Python context.
+:mod:`xrtQook` features a parallelly updated help panel that, unlike the main
+documentation, provides a complete list of parameters for the used classes,
+also including those from the parental classes. :mod:`xrtQook` writes/reads the
+recipes of beamlines into/from xml files.
 
-  .. imagezoom:: _images/qookTutor1.png
-     :scale: 60 %
+In the present version, :mod:`xrtQook` does not provide automated generation of
+*scans* and does not create *wave propagation* sequences. For these two tasks,
+the corresponding script parts have to be written manually based on the
+supplied examples and the present documentation.
 
-- In its properties change eMin to 10000-1 and eMax to 10000+1. The middle of
-  this range will be used to automatically align crystals (one crystal in this
-  example). Blue color indicates non-default values. These will be necessarily
-  included into the generated script. All the default-valued parameters do not
-  propagate into the script.
+See a short :ref:`tutorial for xrtQook <qook>`.
 
-  .. imagezoom:: _images/qookTutor2.png
-     :scale: 60 %
-
-- In Materials tab create a crystalline material:
-  right click -> Add Material -> CrystalSi. This will create a Si111 crystal at
-  room temperature.
-
-  .. imagezoom:: _images/qookTutor3.png
-     :scale: 60 %
-
-- In Beamline tab right click -> Add OE -> OE. This will add an optical element
-  with a flat surface.
-
-  .. note::
-     The sequence of the inserted optical elements does matter! This sequence
-     determines the order of beam propagation.
-
-  .. imagezoom:: _images/qookTutor4.png
-     :scale: 60 %
-
-- In its properties select the created crystal as 'material', put [0, 20000, 0]
-  as 'center' (i.e. 20 m from source) and "auto" (with or without quotes) as
-  'pitch'.
-- Add a screen to the beamline. Give it [0, 21000, "auto"] as 'center'. Its
-  height -- the last coordinate -- will be automatically calculated from the
-  previous elements.
-
-  .. imagezoom:: _images/qookTutor5.png
-     :scale: 60 %
-
-- Add methods to the beamline elements (with right click):
-
-  a) shine() to the source,
-  b) reflect() to the optical element and select a proper beam for it – the
-     global beam from the source, it has a default name but you may rename it
-     in the shine();
-  c) expose() to the screen and select a proper beam for it – the global beam
-     from the OE, it has a default name but you may rename it in the reflect();
-
-  Red colored words indicate None as a selected beam. If you continue with the
-  script generation, the script will result in a run time error.
-
-  .. imagezoom:: _images/qookTutor6.png
-     :scale: 60 %
-
-- Add a plot in Plots tab and select the local screen beam.
-- Save the beamline layout as xml.
-- Generate python script (the button with a code page and the python logo),
-  save the script and run it.
-- In the console output you can read the actual pitch (Bragg angle) for the
-  crystal and the screen position.
-
-  .. imagezoom:: _images/qookTutor7.png
-     :scale: 60 %
+.. imagezoom:: _images/xrtQook.png
+   :width: 562
+   :height: 302
+   :widthzoom: 1124
+   :heightzoom: 604
 
 """
+
 from __future__ import print_function
 __author__ = "Roman Chernikov, Konstantin Klementiev"
 __date__ = "25 Jun 2017"
@@ -1055,10 +1010,11 @@ Compute Units: {3}\nFP64 Support: {4}'.format(platform.name,
             new_html = re.sub('<body>', spyder_crutch, html_text, 1)
         self.webHelp.setHtml(new_html, qt.QUrl(spyder.CSS_PATH))
         if delegateLink:
+            from . import tutorial
             self.webHelp.page().setLinkDelegationPolicy(1)
             self.webHelp.page().linkClicked.connect(partial(
                 self.showTutorial,
-                __doc__[229:],
+                tutorial.__doc__[229:],
                 "Using xrtQook for script generation",
                 self.xrtQookDir))
         else:
--- xrt/gui/xrtQookStart.py
+++ xrt/gui/xrtQookStart.py
@@ -1,35 +1,7 @@
 # -*- coding: utf-8 -*-
-u"""
-The main interface to xrt is through a python script. Many examples of such
-scripts can be found in the supplied folder ‘examples’. The script imports the
-modules of xrt, instantiates beamline parts, such as synchrotron or geometric
-sources, various optical elements, apertures and screens, specifies required
-materials for reflection, refraction or diffraction, defines plots and sets job
-parameters.
-
-The Qt tool :mod:`xrtQook` takes these ingredients and prepares a ready to use
-script that can be run within the tool itself or in an external Python context.
-:mod:`xrtQook` features a parallelly updated help panel that, unlike the main
-documentation, provides a complete list of parameters for the used classes,
-also including those from the parental classes. :mod:`xrtQook` writes/reads the
-recipes of beamlines into/from xml files.
-
-In the present version, :mod:`xrtQook` does not provide automated generation of
-*scans* and does not create *wave propagation* sequences. For these two tasks,
-the corresponding script parts have to be written manually based on the
-supplied examples and the present documentation.
-
-See a short :ref:`tutorial for xrtQook <qook>`.
-
-.. imagezoom:: _images/xrtQook.png
-   :width: 562
-   :height: 302
-   :widthzoom: 1124
-   :heightzoom: 604
-
-"""
+"""This script launches xrtQook."""
 __author__ = "Roman Chernikov, Konstantin Klementiev"
-__date__ = "08 Mar 2016"
+__date__ = "27 Oct 2017"
 
 import sys
 import os
