fa7b05ad491d44a24a4b45dacd8c7e9a596e93e0
Roman Chernikov <yxrmz@users.noreply.github.com>
2018-04-09 21:33:06 -0600

    fixed the reference to opencl-specific parameter in conventional method

--- xrt/backends/raycing/sources_synchr.py
+++ xrt/backends/raycing/sources_synchr.py
@@ -1506,7 +1506,7 @@ class Undulator(object):
             else:
                 sz = 1 if self.filamentBeam else NRAYS
                 gamma += gamma * self.eEspread * np.random.normal(size=sz)
-        gamma = gamma * np.ones(NRAYS, dtype=self.cl_precisionF)
+        gamma = gamma * np.ones(NRAYS)
         gamma2 = gamma**2
 
         wu = PI * C * 10 / self.L0 / gamma2 * np.ones_like(w) *\
