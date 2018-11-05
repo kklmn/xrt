b792698b4e30d9efa1f89c6648c29403d4fd3936
Roman Chernikov <yxrmz@users.noreply.github.com>
2018-11-05 10:19:02 -0600

    Qook2: removed module description
    To be updated later

--- xrt/gui/xrtQook2/__init__.py
+++ xrt/gui/xrtQook2/__init__.py
@@ -1,34 +1,6 @@
 # -*- coding: utf-8 -*-
 u"""
-.. _qook:
 
-xrtQook -- a GUI for creating a beamline
-----------------------------------------
-
-The main interface to xrt is through a python script. Many examples of such
-scripts can be found in the supplied folder ‘examples’. The script imports the
-modules of xrt, instantiates beamline parts, such as synchrotron or geometric
-sources, various optical elements, apertures and screens, specifies required
-materials for reflection, refraction or diffraction, defines plots and sets job
-parameters.
-
-The Qt tool :mod:`xrtQook` takes these ingredients and prepares a ready to use
-script that can be run within the tool itself or in an external Python context.
-:mod:`xrtQook` features a parallelly updated help panel that provides a
-complete list of parameters for the used classes, also including those from the
-parental classes. :mod:`xrtQook` writes/reads the recipes of beamlines
-into/from xml files.
-
-In the present version, :mod:`xrtQook` does not provide automated generation of
-*scans* and does not create *wave propagation* sequences. For these two tasks,
-the corresponding script parts have to be written manually based on the
-supplied examples and the present documentation.
-
-See a brief :ref:`tutorial for xrtQook <qook_tutorial>`.
-
-.. imagezoom:: _images/xrtQook.png
-   :alt: &ensp;A view of xrtQook with an empty beamline tree on the left and a
-       help panel on the right.
 
 """
 
