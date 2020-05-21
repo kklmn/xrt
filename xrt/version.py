13a09009b5d09ef00c2de97d6a7c13300665d71b
Konstantin Klementiev <konstantin.klementiev@gmail.com>
2020-05-21 22:26:31 +0200

    for release 1.3.4

--- xrt/version.py
+++ xrt/version.py
@@ -1,4 +1,4 @@
 ﻿# -*- coding: utf-8 -*-
-__versioninfo__ = (1, 3, 3)
+__versioninfo__ = (1, 3, 4)
 __version__ = '.'.join(map(str, __versioninfo__))
-__date__ = "11 Mar 2019"
+__date__ = "21 May 2020"
