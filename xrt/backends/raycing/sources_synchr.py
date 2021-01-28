7e90159a9d6601d6f668da6fe668b3e5a5fcc131
Roman Chernikov <yxrmz@users.noreply.github.com>
2021-01-28 17:51:59 -0600

    Bending magnet shine fix

--- xrt/backends/raycing/sources_synchr.py
+++ xrt/backends/raycing/sources_synchr.py
@@ -173,6 +173,16 @@ class SourceBase:
         if (self.dz is not None) and (self._betaZ is None):
             self._betaZ = self.dz**2 / self._eEpsilonZ if self._eEpsilonZ\
                 else 0.
+
+        if (self.dx is None) and (self._betaX is not None):
+            self.dx = np.sqrt(self._eEpsilonX*self._betaX)
+        elif (self.dx is None) and (self._betaX is None):
+            print("Set either dx or betaX!")
+        if (self.dz is None) and (self._betaZ is not None):
+            self.dz = np.sqrt(self._eEpsilonZ*self._betaZ)
+        elif (self.dz is None) and (self._betaZ is None):
+            print("Set either dz or betaZ!")
+
         dxprime, dzprime = None, None
         if dxprime:
             self.dxprime = dxprime
@@ -1095,6 +1105,7 @@ class IntegratedSource(SourceBase):
             if not isOpenCL:
                 print("pyopencl is not available!")
             else:
+                print("1", targetOpenCL)
                 self.ucl = mcl.XRT_CL(
                     r'undulator.cl', targetOpenCL, precisionOpenCL)
                 if self.ucl.lastTargetOpenCL is not None:
@@ -1397,6 +1408,7 @@ class BendingMagnet(SourceBase):
         B0 = kwargs.pop('B0', 1.)
         rho = kwargs.pop('rho', None)
         super(BendingMagnet, self).__init__(*args, **kwargs)
+
         if isinstance(self, Wiggler):
             self.B = K2B * self.K / self.L0
             self.ro = M0 * C**2 * self.gamma / self.B / E0 / 1e6
@@ -1415,28 +1427,21 @@ class BendingMagnet(SourceBase):
 
         if self.isMPW:  # xPrimeMaxAutoReduce
             xPrimeMaxTmp = self.K / self.gamma
-            if self.xPrimeMax > xPrimeMaxTmp:
+            if self._xPrimeMax > xPrimeMaxTmp:
                 print("Reducing xPrimeMax from {0} down to {1} mrad".format(
                       self.xPrimeMax * 1e3, xPrimeMaxTmp * 1e3))
-                self.xPrimeMax = xPrimeMaxTmp
-
-#        if self.filamentBeam:
-#            self.nrepmax = np.floor(rMax / len(np.where(
-#                self.Imax * np.random.rand(rMax) < DistI)[0]))
-
-        """Preparing to calculate the total flux integral"""
-        self.xzE = 4 * (self.E_max-self.E_min) * self.Theta_max * self.Psi_max
-        self.fluxConst = self.Imax * self.xzE
+                self._xPrimeMax = xPrimeMaxTmp
 
     @property
-    def B(self):
-        return self._B
+    def B0(self):
+        return self.B
 
-    @B.setter
-    def B(self, B):
-        self._B = float(B)
+    @B0.setter
+    def B0(self, B):
+        self.B = float(B)
         self.ro = M0 * C**2 * self.gamma / B / E0 / 1e6
-        self._K = B * self.L0 / K2B  # Only for the Wiggler
+        if hasattr(self, 'L0'):
+            self._K = B * self.L0 / K2B  # Only for the Wiggler
         self.needReset = True
         # Need to recalculate the integration parameters
 
@@ -1447,8 +1452,9 @@ class BendingMagnet(SourceBase):
     @rho.setter
     def rho(self, rho):
         self.ro = rho
-        self._B = M0 * C**2 * self.gamma / rho / E0 / 1e6
-        self._K = self.B * self.L0 / K2B
+        self.B = M0 * C**2 * self.gamma / rho / E0 / 1e6
+        if hasattr(self, 'L0'):
+            self._K = self.B * self.L0 / K2B
         self.needReset = True
         # Need to recalculate the integration parameters
 
@@ -1496,6 +1502,256 @@ class BendingMagnet(SourceBase):
         return (Amp2Flux * (np.abs(ampS)**2 + np.abs(ampP)**2),
                 np.sqrt(Amp2Flux) * ampS,
                 np.sqrt(Amp2Flux) * ampP)
+
+    def shine(self, toGlobal=True, withAmplitudes=True, fixedEnergy=False,
+              accuBeam=None):
+        u"""
+        Returns the source beam. If *toGlobal* is True, the output is in
+        the global system. If *withAmplitudes* is True, the resulted beam
+        contains arrays Es and Ep with the *s* and *p* components of the
+        electric field.
+
+
+        .. Returned values: beamGlobal
+        """
+        if self.needReset:
+            self.reset()
+        if self.bl is not None:
+            try:
+                self.bl._alignE = float(self.bl.alignE)
+            except ValueError:
+                self.bl._alignE = 0.5 * (self.eMin + self.eMax)
+
+        if self.uniformRayDensity:
+            withAmplitudes = True
+
+        bo = None
+        length = 0
+        seeded = np.long(0)
+        seededI = 0.
+        np.seterr(invalid='warn')
+        np.seterr(divide='warn')
+        mcRays = np.long(self.nrays * 1.2) if not self.uniformRayDensity else\
+            self.nrays
+        if self.filamentBeam:
+            if accuBeam is None:
+                rE = np.random.random_sample() *\
+                    float(self.E_max - self.E_min) + self.E_min
+                if self.isMPW:
+                    sigma_r2 = 2 * (CHeVcm/rE*10*self.L0*self.Np) / PI2**2
+                    sourceSIGMAx = self.dx
+                    sourceSIGMAz = self.dz
+                    rTheta0 = np.random.random_sample() *\
+                        (self.Theta_max - self.Theta_min) + self.Theta_min
+                    ryNp = 0.5 * self.L0 *\
+                        (np.arccos(rTheta0 * self.gamma / self.K) / PI) +\
+                        0.5 * self.L0 *\
+                        np.random.random_integers(0, int(2*self.Np - 1))
+                    rY = ryNp - 0.5*self.L0*self.Np
+                    if (ryNp - 0.25*self.L0 <= 0):
+                        rY += self.L0*self.Np
+                    rX = self.X0 * np.sin(PI2 * rY / self.L0) +\
+                        sourceSIGMAx * np.random.standard_normal()
+                    rY -= 0.25 * self.L0
+                    rZ = sourceSIGMAz * np.random.standard_normal()
+                else:
+                    rZ = self.dz * np.random.standard_normal()
+                    rTheta0 = np.random.random_sample() *\
+                        (self.Theta_max - self.Theta_min) + self.Theta_min
+                    R1 = self.dx * np.random.standard_normal() +\
+                        self.ro * 1000.
+                    rX = -R1 * np.cos(rTheta0) + self.ro*1000.
+                    rY = R1 * np.sin(rTheta0)
+                dtheta = self.dxprime * np.random.standard_normal()
+                dpsi = self.dzprime * np.random.standard_normal()
+            else:
+                rE = accuBeam.E[0]
+                rX = accuBeam.x[0]
+                rY = accuBeam.y[0]
+                rZ = accuBeam.z[0]
+                dtheta = accuBeam.filamentDtheta
+                dpsi = accuBeam.filamentDpsi
+        if fixedEnergy:
+            rE = fixedEnergy
+
+        nrep = 0
+        rep_condition = True
+#        while length < self.nrays:
+        while rep_condition:
+            """Preparing 4 columns of random numbers
+            0: Energy
+            1: Theta / horizontal
+            2: Psi / vertical
+            3: Monte-Carlo discriminator"""
+            rnd_r = np.random.rand(mcRays, 4)
+            seeded += mcRays
+            if self.filamentBeam:
+                rThetaMin = np.max((self.Theta_min, rTheta0 - 1. / self.gamma))
+                rThetaMax = np.min((self.Theta_max, rTheta0 + 1. / self.gamma))
+                rTheta = (rnd_r[:, 1]) * (rThetaMax - rThetaMin) +\
+                    rThetaMin
+                rE *= np.ones(mcRays)
+            else:
+                rE = rnd_r[:, 0] * float(self.E_max - self.E_min) +\
+                    self.E_min
+                rTheta = (rnd_r[:, 1]) * (self.Theta_max - self.Theta_min) +\
+                    self.Theta_min
+            rPsi = rnd_r[:, 2] * (self.Psi_max - self.Psi_min) +\
+                self.Psi_min
+            Intensity, mJss, mJpp = self.build_I_map(rE, rTheta, rPsi)
+
+            if self.uniformRayDensity:
+                seededI += self.nrays * self.xzE
+            else:
+                seededI += Intensity.sum() * self.xzE
+
+            tmp_max = np.max(Intensity)
+            if tmp_max > self.Imax:
+                self.Imax = tmp_max
+                self.fluxConst = self.Imax * self.xzE
+                if raycing._VERBOSITY_ > 10:
+                    imax = np.argmax(Intensity)
+                    print(self.Imax, imax, rE[imax], rTheta[imax], rPsi[imax])
+
+            if self.uniformRayDensity:
+                I_pass = slice(None)
+                npassed = mcRays
+            else:
+                I_pass =\
+                    np.where(self.Imax * rnd_r[:, 3] < Intensity)[0]
+                npassed = len(I_pass)
+            if npassed == 0:
+                print('No good rays in this seed!'
+                      ' {0} of {1} rays in total so far...'.format(
+                          length, self.nrays))
+                continue
+
+            bot = Beam(npassed, withAmplitudes=withAmplitudes)
+            bot.state[:] = 1  # good
+
+            bot.E[:] = rE[I_pass]
+
+            Theta0 = rTheta[I_pass]
+            Psi0 = rPsi[I_pass]
+
+            if not self.filamentBeam:
+                if self.dxprime > 0:
+                    dtheta = np.random.normal(0, self.dxprime, npassed)
+                else:
+                    dtheta = 0
+                if not self.isMPW:
+                    dtheta += np.random.normal(0, 1/self.gamma, npassed)
+
+                if self.dzprime > 0:
+                    dpsi = np.random.normal(0, self.dzprime, npassed)
+                else:
+                    dpsi = 0
+
+            bot.a[:] = np.tan(Theta0 + dtheta)
+            bot.c[:] = np.tan(Psi0 + dpsi)
+
+            intensS = (mJss[I_pass] * np.conj(mJss[I_pass])).real
+            intensP = (mJpp[I_pass] * np.conj(mJpp[I_pass])).real
+            if self.uniformRayDensity:
+                sSP = 1.
+            else:
+                sSP = intensS + intensP
+            # as by Walker and by Ellaume; SPECTRA's value is two times
+            # smaller:
+
+            if self.isMPW:
+                sigma_r2 = 2 * (CHeVcm/bot.E*10 * self.L0*self.Np) / PI2**2
+                bot.sourceSIGMAx = np.sqrt(self.dx**2 + sigma_r2)
+                bot.sourceSIGMAz = np.sqrt(self.dz**2 + sigma_r2)
+                if self.filamentBeam:
+                    bot.z[:] = rZ
+                    bot.x[:] = rX
+                    bot.y[:] = rY
+                else:
+                    bot.y[:] = ((np.arccos(Theta0*self.gamma/self.K) / PI) +
+                                np.random.randint(
+                                    -int(self.Np), int(self.Np), npassed) -
+                                0.5) * 0.5 * self.L0
+                    bot.x[:] = self.X0 * np.sin(PI2 * bot.y / self.L0) +\
+                        np.random.normal(0., bot.sourceSIGMAx, npassed)
+                    bot.z[:] = np.random.normal(0., bot.sourceSIGMAz, npassed)
+                bot.Jsp[:] = np.zeros(npassed)
+            else:
+                if self.filamentBeam:
+                    bot.z[:] = rZ
+                    bot.x[:] = rX
+                    bot.y[:] = rY
+                else:
+                    if self.dz > 0:
+                        bot.z[:] = np.random.normal(0., self.dz, npassed)
+                    if self.dx > 0:
+                        R1 = np.random.normal(self.ro*1e3, self.dx, npassed)
+                    else:
+                        R1 = self.ro * 1e3
+                    bot.x[:] = -R1 * np.cos(Theta0) + self.ro*1000.
+                    bot.y[:] = R1 * np.sin(Theta0)
+
+                bot.Jsp[:] = np.array(
+                    np.where(sSP,
+                             mJss[I_pass] * np.conj(mJpp[I_pass]) / sSP,
+                             sSP), dtype=complex)
+
+            bot.Jss[:] = np.where(sSP, intensS / sSP, sSP)
+            bot.Jpp[:] = np.where(sSP, intensP / sSP, sSP)
+
+            if withAmplitudes:
+                bot.Es[:] = mJss[I_pass]
+                bot.Ep[:] = mJpp[I_pass]
+
+            if bo is None:
+                bo = bot
+            else:
+                bo.concatenate(bot)
+            length = len(bo.a)
+            if raycing._VERBOSITY_ > 0:
+                print("{0} rays of {1}".format(length, self.nrays))
+                try:
+                    if self.bl is not None:
+                        if self.bl.flowSource == 'Qook' and\
+                                self.bl.statusSignal is not None:
+                            ptg = (self.bl.statusSignal[1] +
+                                   float(length) / float(self.nrays)) /\
+                                      self.bl.statusSignal[2]
+                            self.bl.statusSignal[0].emit(
+                                (ptg, self.bl.statusSignal[3]))
+                except:
+                    pass
+            if self.filamentBeam:
+                nrep += 1
+                rep_condition = nrep < self.nrepmax
+            else:
+                rep_condition = length < self.nrays
+            if self.uniformRayDensity:
+                rep_condition = False
+            if raycing._VERBOSITY_ > 0:
+                sys.stdout.flush()
+
+        if length >= self.nrays:
+            bo.accepted = length * self.fluxConst
+            bo.acceptedE = bo.E.sum() * self.fluxConst * SIE0
+            bo.seeded = seeded
+            bo.seededI = seededI
+        if length > self.nrays and not self.filamentBeam:
+            bo.filter_by_index(slice(0, np.long(self.nrays)))
+        if self.filamentBeam:
+            bo.filamentDtheta = dtheta
+            bo.filamentDpsi = dpsi
+        norm = np.sqrt(bo.a**2 + 1.0 + bo.c**2)
+        bo.a /= norm
+        bo.b /= norm
+        bo.c /= norm
+        if self.pitch or self.yaw:
+            raycing.rotate_beam(bo, pitch=self.pitch, yaw=self.yaw)
+        if toGlobal:  # in global coordinate system:
+            raycing.virgin_local_to_global(self.bl, bo, self.center)
+        raycing.append_to_flow(self.shine, [bo],
+                               inspect.currentframe())
+        return bo
 
 
 class Wiggler(BendingMagnet):
