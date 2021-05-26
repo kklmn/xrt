fix angular units parsing

xrt/backends/raycing/__init__.py
@@ -140,7 +140,7 @@ __module__ = "raycing"
 __author__ = "Konstantin Klementiev, Roman Chernikov"
 __date__ = "26 Mar 2016"
 
-_DEBUG_ = False  # If False, exceptions inside the module are ignored
+_DEBUG_ = True  # If False, exceptions inside the module are ignored
 _VERBOSITY_ = 10   # [0-100] Regulates the level of diagnostics printout
 
 try:  # for Python 3 compatibility:
@@ -697,25 +697,27 @@ def get_output(plot, beamsReturnedBy_run_process):
         locAccepted, locAcceptedE, locSeeded, locSeededI
 
 
-def auto_units_angle(angle):
+def auto_units_angle(angle, defaultFactor=1.):
     if isinstance(angle, basestring):
         if len(re.findall("auto", angle)) > 0:
             return angle
         elif len(re.findall("mrad", angle)) > 0:
-            return eval(angle.split("m")[0].strip())*1e-3
+            return float(angle.split("m")[0].strip())*1e-3
         elif len(re.findall("urad", angle)) > 0:
-            return eval(angle.split("u")[0].strip())*1e-6
+            return float(angle.split("u")[0].strip())*1e-6
         elif len(re.findall("nrad", angle)) > 0:
-            return eval(angle.split("n")[0].strip())*1e-9
+            return float(angle.split("n")[0].strip())*1e-9
         elif len(re.findall("rad", angle)) > 0:
-            return eval(angle.split("r")[0].strip())
+            return float(angle.split("r")[0].strip())
         elif len(re.findall("deg", angle)) > 0:
-            return np.radians(eval(angle.split("d")[0].strip()))
+            return np.radians(float(angle.split("d")[0].strip()))
         else:
             print("Could not identify the units")
             return angle
-    else:
+    elif angle is None:
         return angle
+    else:
+        return angle * defaultFactor
 
 
 def append_to_flow(meth, bOut, frame):

xrt/backends/raycing/sources_synchr.py
@@ -135,8 +135,8 @@ class SourceBase:
                 bl.oesDict[self.name] = [self, 0]
 
         self.center = center  # 3D point in global system
-        self.pitch = raycing.auto_units_angle(pitch)
-        self.yaw = raycing.auto_units_angle(yaw)
+        self._pitch = raycing.auto_units_angle(pitch)
+        self._yaw = raycing.auto_units_angle(yaw)
         self.nrays = np.long(nrays)
 
         self.R0 = R0
@@ -231,19 +231,19 @@ class SourceBase:
 
     @property
     def pitch(self):
-        return self.pitch
+        return self._pitch
 
     @pitch.setter
     def pitch(self, pitch):
-        self.pitch = raycing.auto_units_angle(pitch)
+        self._pitch = raycing.auto_units_angle(pitch)
 
     @property
     def yaw(self):
-        return self.yaw
+        return self._yaw
 
     @yaw.setter
     def yaw(self, yaw):
-        self.yaw = raycing.auto_units_angle(yaw)
+        self._yaw = raycing.auto_units_angle(yaw)
 
     @property
     def eSigmaX(self):
@@ -2511,7 +2511,6 @@ class SourceFromField(IntegratedSource):
 
         betam = betazav[-1]
         ab = 0.5 / np.pi / betam
-        print("R0=",R0)
 
         if self.filamentBeam:
             emcg0 = EMC/gamma[0]
@@ -2544,12 +2543,6 @@ class SourceFromField(IntegratedSource):
                 clKernel, scalarArgsTest, slicedROArgs, nonSlicedROArgs,
                 slicedRWArgs, None, NRAYS)
 
-            self.beta[0] *= emcg0
-            self.beta[1] *= emcg0
-            self.trajectory[0] *= emcg0
-            self.trajectory[1] *= emcg0
-            smtrm = 1./gamma[0]**2 + self.beta[0]**2 + self.beta[1]**2
-            self.trajectory[2]
         else:
             ab = 0.5 / np.pi / (1. - 0.5/gamma**2 + betam*EMC**2/gamma**2)
 