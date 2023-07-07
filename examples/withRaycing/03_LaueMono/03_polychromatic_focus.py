2a2d9c76769a0e802b92047b701ede40e62a9400
kklmn <konstantin.klementiev@gmail.com>
2023-07-07 13:48:52 +0200

    Update 03_polychromatic_focus.py

--- examples/withRaycing/03_LaueMono/03_polychromatic_focus.py
+++ examples/withRaycing/03_LaueMono/03_polychromatic_focus.py
@@ -7,8 +7,6 @@ __date__ = "2023-03-29"
 Bent Laue Crystal Polychromatic Focus
 
 
-
-
 """
 
 import numpy as np
@@ -17,13 +15,13 @@ import xrt.backends.raycing.sources as rsources
 import xrt.backends.raycing.screens as rscreens
 import xrt.backends.raycing.materials as rmats
 import xrt.backends.raycing.oes as roes
-import xrt.backends.raycing.apertures as rapts
+# import xrt.backends.raycing.apertures as rapts
 import xrt.backends.raycing.run as rrun
 import xrt.backends.raycing as raycing
 import xrt.plotter as xrtplot
 import xrt.runner as xrtrun
 
-#showIn3D = False
+# showIn3D = False
 showIn3D = True
 
 nrays = 250000
@@ -38,17 +36,17 @@ sourceDxPrime, sourceDzPrime = 0, 0
 E0, dE = 30000, 25
 eLims = [E0-2*dE, E0+dE]
 eLimsSource = np.linspace(E0-2*dE, E0+dE, 7)
-#eLims = [34850, 35100]
-pLaueSCM, qLaueSCM  = 200., 200
-#Rbend = 3.00e3  # Overbend
-#Rbend = 5.00e3  # Optimal bend
+# eLims = [34850, 35100]
+pLaueSCM, qLaueSCM = 200., 200
+# Rbend = 3.00e3  # Overbend
+# Rbend = 5.00e3  # Optimal bend
 Rbend = 25.0e3  # Underbend
-#Rbend = 1e6  # (Almost) Plain crystal
-#Rbend = [10e3, 20e3, 40e3, 1e6]
-#Rbend = [3e3, 5e3, 10e3, 20e3, 40e3, 1e6]
-#precision='float64'
-#targetOpenCL=None
-#targetOpenCL=(0, 0)
+# Rbend = 1e6  # (Almost) Plain crystal
+# Rbend = [10e3, 20e3, 40e3, 1e6]
+# Rbend = [3e3, 5e3, 10e3, 20e3, 40e3, 1e6]
+# precision='float64'
+# targetOpenCL=None
+# targetOpenCL=(0, 0)
 
 crystalSi01 = rmats.CrystalSi(
     t=thickness,
@@ -58,7 +56,6 @@ crystalSi01 = rmats.CrystalSi(
 alpha = np.radians(-35)
 thetaB = crystalSi01.get_Bragg_angle(E0)
 pitch = np.pi/2+thetaB+alpha
-
 
 crystalSi02 = rmats.CrystalSi(
     t=thickness,
@@ -70,6 +67,7 @@ crystalSi02 = rmats.CrystalSi(
 tp_x = np.array([-1, -1, 1, 1])
 tp_y = np.array([-1, 1, -1, 1])
 tp_z = np.array([0, 0, 0, 0])
+
 
 def build_beamline():
     beamLine = raycing.BeamLine()
@@ -86,8 +84,8 @@ def build_beamline():
         dzprime=sourceDzPrime,
         energies=eLims,
         distE='flat',
-#        energies=eLimsSource,
-#        distE='lines',
+        # energies=eLimsSource,
+        # distE='lines',
         )
 
     beamLine.lauePlate01 = roes.BentLaueCylinder(
@@ -129,11 +127,9 @@ def build_beamline():
         dzprime=sourceDzPrime,
         energies=eLims,
         distE='flat',
-#        energies=eLimsSource,
-#        distE='lines'
+        # energies=eLimsSource,
+        # distE='lines'
         )
-
-
 
     beamLine.lauePlate02 = roes.BentLaueCylinder(
         name='LaueCylinderVD',
@@ -146,10 +142,10 @@ def build_beamline():
         limPhysX=xtallimsx,
         limPhysY=xtallimsy)
 
-    print("local_z", beamLine.lauePlate02.local_z(tp_x, tp_y))
-    print("local_n", beamLine.lauePlate02.local_n(tp_x, tp_y))
-    print("local_n_depth", beamLine.lauePlate02.local_n_depth(tp_x, tp_y, tp_z))
-    sys.exit()
+    # print("local_z", beamLine.lauePlate02.local_z(tp_x, tp_y))
+    # print("local_n", beamLine.lauePlate02.local_n(tp_x, tp_y))
+    # print("local_n_depth", beamLine.lauePlate02.local_n_depth(tp_x, tp_y, tp_z))
+    # sys.exit()
 
     beamLine.Mirror02 = roes.OE(
         bl=beamLine,
@@ -173,8 +169,8 @@ def build_beamline():
 def run_process(beamLine):
     geometricSource01beamGlobal01 = beamLine.geometricSource01.shine()
 
-    lauePlate01beamGlobal01, lauePlate01beamLocal01 = beamLine.lauePlate01.reflect(
-        beam=geometricSource01beamGlobal01)
+    lauePlate01beamGlobal01, lauePlate01beamLocal01 = \
+        beamLine.lauePlate01.reflect(beam=geometricSource01beamGlobal01)
 
     mirror01beamGlobal01, mirror01beamLocal01 = beamLine.Mirror01.reflect(
         beam=lauePlate01beamGlobal01)
@@ -187,8 +183,8 @@ def run_process(beamLine):
 
     geometricSource02beamGlobal01 = beamLine.geometricSource02.shine()
 
-    lauePlate02beamGlobal01, lauePlate02beamLocal01 = beamLine.lauePlate02.reflect(
-        beam=geometricSource02beamGlobal01)
+    lauePlate02beamGlobal01, lauePlate02beamLocal01 = \
+        beamLine.lauePlate02.reflect(beam=geometricSource02beamGlobal01)
 
     mirror02beamGlobal01, mirror02beamLocal01 = beamLine.Mirror02.reflect(
         beam=lauePlate02beamGlobal01)
@@ -220,8 +216,6 @@ def run_process(beamLine):
     if showIn3D:
         beamLine.prepare_flow()
     return outDict
-
-
 rrun.run_process = run_process
 
 
@@ -337,6 +331,7 @@ def main():
 
     if showIn3D:
         beamLine.glow()
+        return
 
     plots = define_plots()
     xrtrun.run_ray_tracing(
@@ -344,7 +339,7 @@ def main():
         backend=r"raycing",
         repeats=40,
         processes='half',
-#        generator=plot_generator,
+        # generator=plot_generator,
         beamLine=beamLine)
 
 
