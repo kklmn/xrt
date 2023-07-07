d2b821b8ac1a9758f1e50e6f2635aa3ed87d0601
Roman Chernikov <yxrmz@users.noreply.github.com>
2023-07-07 13:19:51 -0600

    ZMQ OpenCL server docs

--- tests/raycing/RemoteOpenCLCalculation/zmq_server.py
+++ tests/raycing/RemoteOpenCLCalculation/zmq_server.py
@@ -3,7 +3,53 @@
 OpenCL Server
 ---------
 
-Server for remote OpenCL calculations.
+This example contains a Python script designed to run on a GPU server,
+leveraging ZeroMQ (ZMQ) for efficient data transfer. The script acts as a
+remote accelerator device, receiving data from a client Python script,
+performing calculations on the GPU, and returning the results for plotting on
+a local computer. 
+Users can seamlessly execute their scripts in their favorite IDE,
+offloading resource-intensive calculations to a remote server over the network.
+The only trade-off is the potential delay due to data transfer, which is
+outweighed by the benefits when local computations take longer than data
+transfer time. Furthermore, the local graphical user interface (GUI) remains
+responsive without freezes or issues caused by high GPU/CPU loads.
+This script now supports all acceleration scenarios:
+sycnhrotron sources, wave propagation, bent crystals and multilayers
+reflectivity.
+
+Script Components
+The GPU accelerator script is comprised of two files:
+zmq_server.py: The server script serves as the main component, responsible for
+receiving data and kernel names from the client. It listens on a predefined
+port, processes the received package, executes the specified kernel on the GPU,
+and sends the computed data back to the client. This server script can be
+executed independently or in conjunction with the queue manager.
+
+queue_device.py: The queue manager script facilitates the handling of multiple
+user requests and the distribution of computational tasks across multiple
+servers. It provides scalability and load balancing capabilities. The queue
+manager can be executed on the same machine as the server or on a dedicated
+node. However, when running the queue manager on a separate node, data transfer
+times may increase.
+
+Running the Script
+To execute the GPU accelerator script, follow these steps:
+
+Set up the GPU server environment with the necessary dependencies, including
+pyzmq, xrt and underlying dependencies (numpy, scipy, matplotlib, pyopencl).
+Start the server script, either as a standalone process or in conjunction with
+the queue manager, based on your specific requirements.
+
+Ensure that the *client* Python script is configured to connect to the correct
+server address and port:
+    for standalone server:
+    targetOpenCL="GPU_SERVER_ADDRESS:15560"
+
+    for queue manager:
+    targetOpenCL="QUEUE_MANAGER_ADDRESS:15559"
+
+
 """
 
 __author__ = "Roman Chernikov, Konstantin Klementiev"
@@ -47,7 +93,7 @@ undulatorKernels = ['undulator', 'undulator_taper', 'undulator_nf',
                     'get_trajectory', 'custom_field']
 oesKernels = ['reflect_crystal']
 
-
+# Please specify here the OpenCL device installed on the server
 targetOpenCL = 'GPU'
 
 matCL32 = mcl.XRT_CL(r'materials.cl', precisionOpenCL='float32',
@@ -66,10 +112,12 @@ OE64 = OE(precisionOpenCL='float64', targetOpenCL=targetOpenCL)
 oeCL32 = OE32.ucl
 oeCL64 = OE64.ucl
 
-
 port = "15560"
 context = zmq.Context()
 socket = context.socket(zmq.REP)
+
+# Replace 'localhost' with the queue manager address if not run on same machine
+# Replace 'localhost' with '*' for standalone mode
 socket.connect("tcp://localhost:%s" % port)
 
 while True:
