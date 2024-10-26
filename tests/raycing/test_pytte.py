ec4419611d3333ab464ec7c24ced703b51ba0b89
Roman Chernikov <yxrmz@users.noreply.github.com>
2024-10-26 01:17:41 -0400

    Update test_pytte.py

--- tests/raycing/test_pytte.py
+++ tests/raycing/test_pytte.py
@@ -1,5 +1,6 @@
 import numpy as np
-import os, sys; sys.path.append(os.path.join('..', '..'))  # analysis:ignore
+import os, sys;
+sys.path.append(os.path.join('..', '..'))  # analysis:ignore
 import xrt.backends.raycing.materials as rm
 import xrt.backends.raycing.materials_crystals as rmc
 import xrt.backends.raycing as raycing
@@ -22,7 +23,6 @@ except ImportError:
     isOpenCL = False
 
 
-
 case1 = {
     "name": "Bragg Flat",
     "crystal": "Si", # select from ('Si', 'Ge', 'Diamond', 'InSb', 'AlphaQuartz', 'Sapphire')
@@ -34,7 +34,7 @@ case1 = {
     "bending_Rm": np.inf,  # meters
     "bending_Rs": np.inf,  # meters
     "energy": 9000,  # eV
-    "theta_array": np.linspace(-50, 150, 200),  # microrad
+    "theta_array": np.linspace(-50, 150, 800),  # microrad
     "polarization": "sigma",  # or "pi"
     "precision": "float32"  # Only for opencl backend. "float64" for Laue
     }
@@ -44,40 +44,74 @@ case2 = {
     "crystal": "Si", # select from ('Si', 'Ge', 'Diamond', 'InSb', 'AlphaQuartz', 'Sapphire')
     "geometry": "Bragg reflected",  # Combination of Bragg/Laue reflected/transmitted
     "hkl": [1, 1, 1],
-    "thickness": 1.,  # mm
+    "thickness": 0.75,  # mm
     "asymmetry": 5.,  # degrees
     "in_plane_rotation": 0.,  # degrees
     "bending_Rm": 10.,  # meters
     "bending_Rs": np.inf,  # meters
-    "energy": 9000,  # eV
-    "theta_array": np.linspace(-50, 150, 200),  # microrad
+    "energy": 9000.,  # eV
+    "theta_array": np.linspace(-50, 150, 800),  # microrad
     "polarization": "sigma",  # or "pi"
     "precision": "float32"  # Only for opencl backend. "float64" for Laue
     }
 
 case3 = {
     "name": "Bragg 2D bent",
-    "crystal": "Si", # select from ('Si', 'Ge', 'Diamond', 'InSb', 'AlphaQuartz', 'Sapphire')
+    "crystal": "Ge", # select from ('Si', 'Ge', 'Diamond', 'InSb', 'AlphaQuartz', 'Sapphire')
     "geometry": "Bragg reflected",  # Combination of Bragg/Laue reflected/transmitted
     "hkl": [3, 3, 3],
-    "thickness": 3.,  # mm
+    "thickness": 0.5,  # mm
     "asymmetry": -2.,  # degrees
     "in_plane_rotation": 0.,  # degrees
     "bending_Rm": 10.,  # meters
     "bending_Rs": 10,  # meters
     "energy": 19000,  # eV
-    "theta_array": np.linspace(-50, 150, 200),  # microrad
+    "theta_array": np.linspace(-150, 50, 800),  # microrad
     "polarization": "sigma",  # or "pi"
     "precision": "float32"  # Only for opencl backend. "float64" for Laue
     }
+
+case4 = {
+    "name": "Laue 1D bent",
+    "crystal": "Si", # select from ('Si', 'Ge', 'Diamond', 'InSb', 'AlphaQuartz', 'Sapphire')
+    "geometry": "Laue reflected",  # Combination of Bragg/Laue reflected/transmitted
+    "hkl": [1, 1, 1],
+    "thickness": 0.5,  # mm
+    "asymmetry": -12.,  # degrees
+    "in_plane_rotation": 60.,  # degrees
+    "bending_Rm": 10.,  # meters
+    "bending_Rs": np.inf,  # meters
+    "energy": 29000,  # eV
+    "theta_array": np.linspace(-100, 100, 1500),  # microrad
+    "polarization": "sigma",  # or "pi"
+    "precision": "float64"  # Only for opencl backend. "float64" for Laue
+    }
+
+case5 = {
+    "name": "Laue 2D bent",
+    "crystal": "AlphaQuartz", # select from ('Si', 'Ge', 'Diamond', 'InSb', 'AlphaQuartz', 'Sapphire')
+    "geometry": "Laue reflected",  # Combination of Bragg/Laue reflected/transmitted
+    "hkl": [1, 1, 1],
+    "thickness": 2.,  # mm
+    "asymmetry": 22.,  # degrees
+    "in_plane_rotation": 60.,  # degrees
+    "bending_Rm": 20.,  # meters
+    "bending_Rs": 20,  # meters
+    "energy": 60000,  # eV
+    "theta_array": np.linspace(-100, 100, 1500),  # microrad
+    "polarization": "sigma",  # or "pi"
+    "precision": "float64"  # Only for opencl backend. "float64" for Laue
+    }
+
 
 def run_pytte_xrt_opencl(name, crystal, geometry, hkl, thickness, asymmetry,
                          in_plane_rotation, bending_Rm, bending_Rs, energy,
                          theta_array, polarization, precision):
     alpha = np.radians(asymmetry)
     crInstance = getattr(rmc, crystal)(hkl=hkl,
-                                      geom=geometry,
-                                      t=thickness)
+                                       geom=geometry,
+                                       t=thickness,
+                                       useTT=True)
 
     thetaCenter = np.arcsin(rm.ch / (2*crInstance.d*energy))
     E_in = np.ones_like(theta_array) * energy
@@ -86,7 +120,6 @@ def run_pytte_xrt_opencl(name, crystal, geometry, hkl, thickness, asymmetry,
     s0 = (np.zeros_like(theta), np.cos(theta+alpha), -np.sin(theta+alpha))
     sh = (np.zeros_like(theta), np.cos(theta-alpha), np.sin(theta-alpha))
     if geometry.startswith('Bragg'):
-        precision = "float32"
         n = (0, 0, 1)  # outward surface normal
     else:
         n = (0, -1, 0)  # outward surface normal
@@ -99,12 +132,12 @@ def run_pytte_xrt_opencl(name, crystal, geometry, hkl, thickness, asymmetry,
                        precisionOpenCL=precision,
                        targetOpenCL=targetOpenCL)
 
-    ampS, ampP = crInstance.get_amplitude_pytte(E_in, gamma0, gammah, hns0,
-                                                ucl=matCL, alphaAsym=alpha,
-                                                inPlaneRotation=in_plane_rotation,
-                                                Ry=bending_Rm*1e3,
-                                                Rx=bending_Rs*1e3)
+    ampS, ampP = crInstance.get_amplitude_pytte(
+            E_in, gamma0, gammah, hns0, ucl=matCL, alphaAsym=alpha,
+            inPlaneRotation=in_plane_rotation,
+            Ry=bending_Rm*1e3, Rx=bending_Rs*1e3)
     return np.abs(ampS)**2 if polarization == "sigma" else np.abs(ampP)**2
+
 
 def run_pytte_xrt_cpu(name, crystal, geometry, hkl, thickness, asymmetry,
                       in_plane_rotation, bending_Rm, bending_Rs, energy,
@@ -113,62 +146,65 @@ def run_pytte_xrt_cpu(name, crystal, geometry, hkl, thickness, asymmetry,
     alpha = np.radians(asymmetry)
     geotag = 0 if geometry.startswith('B') else np.pi*0.5
     crInstance = getattr(rmc, crystal)(hkl=hkl,
-                                      geom=geometry,
-                                      t=thickness)
+                                       geom=geometry,
+                                       t=thickness)
 
     refl = geometry.endswith("lected")
 
     ttx = TTcrystalX(crystal=crystal,
-                    hkl=hkl,
-                    thickness=QuantityX(thickness*1e3,'um'),
-                    debye_waller=1,
-                    xrt_crystal=crInstance,
-                    Rx=QuantityX(bending_Rm, 'm'),
-                    Ry=QuantityX(bending_Rs, 'm'),
-                    asymmetry=QuantityX(alpha+geotag, 'rad'),
-                    in_plane_rotation=QuantityX(in_plane_rotation, 'deg'))
+                     hkl=hkl,
+                     thickness=QuantityX(thickness, 'mm'),
+                     debye_waller=1,
+                     xrt_crystal=crInstance,
+                     Rx=QuantityX(bending_Rm, 'm'),
+                     Ry=QuantityX(bending_Rs, 'm'),
+                     asymmetry=QuantityX(alpha+geotag, 'rad'),
+                     in_plane_rotation=QuantityX(in_plane_rotation, 'deg')
+                     )
 
-    tts = TTscanX(constant=QuantityX(energy,'eV'),
+    tts = TTscanX(constant=QuantityX(energy, 'eV'),
                   scan=QuantityX(theta_array, 'urad'),
                   polarization=polarization)
 
-    scan_tt_s=TakagiTaupinX(ttx, tts)
+    scan_tt_s = TakagiTaupinX(ttx, tts)
     scan_vector, R, T, curSD = scan_tt_s.run()
     return R if refl else T
 
+
 def run_pytte_original(name, crystal, geometry, hkl, thickness, asymmetry,
-                           in_plane_rotation, bending_Rm, bending_Rs, energy,
-                           theta_array, polarization, precision):
+                       in_plane_rotation, bending_Rm, bending_Rs, energy,
+                       theta_array, polarization, precision):
     alpha = np.radians(asymmetry)
     geotag = 0 if geometry.startswith('B') else np.pi*0.5
     refl = geometry.endswith("lected")
 
     ttx = TTcrystal(crystal=crystal,
                     hkl=hkl,
-                    thickness=Quantity(thickness*1e3,'um'),
+                    thickness=Quantity(thickness, 'mm'),
                     debye_waller=1,
-#                    xrt_crystal=crInstance,
                     Rx=Quantity(bending_Rm, 'm'),
                     Ry=Quantity(bending_Rs, 'm'),
                     asymmetry=Quantity(alpha+geotag, 'rad'),
-                    in_plane_rotation=Quantity(in_plane_rotation, 'deg'))
+                    in_plane_rotation=Quantity(in_plane_rotation, 'deg')
+                    )
 
-    tts = TTscan(constant=Quantity(energy,'eV'),
+    tts = TTscan(constant=Quantity(energy, 'eV'),
                  scan=Quantity(theta_array, 'urad'),
                  polarization=polarization)
 
-    scan_tt_s=TakagiTaupin(ttx, tts)
+    scan_tt_s = TakagiTaupin(ttx, tts)
     scan_vector, R, T = scan_tt_s.run()
+    # multiplier = gamma0/gammah  # !!!
     return R if refl else T
 
 
 if __name__ == '__main__':
-#    plt.ion()
-    for calcParams in [case1, case2]: #, case3]:
+
+    for calcParams in [case1, case2, case3, case4, case5]:
         plt.figure(calcParams["name"])
         for func in [
                      run_pytte_xrt_opencl,
-#                     run_pytte_xrt_cpu,
+                     run_pytte_xrt_cpu,
                      run_pytte_original
                      ]:
             data = func(**calcParams)
