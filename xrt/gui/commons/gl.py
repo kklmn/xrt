dce6dc8c90bdb1aafa72ca2ce633a7ef14e938ff
Roman Chernikov <yxrmz@users.noreply.github.com>
2024-12-01 01:15:46 -0500

    New rendering pipeline
    Improvements:
    - discarded GLUT
    - shader-based rendering
    - significantly improved OE rendering performance
    - significantly improved selection performance
    - support for custom fonts
    - object highlighting/selection
    - better beam rendering
    - fixed VBO warnings flooding the terminal
    
    Missing for now (in progress):
    - orientation arrows
    - slits, screens, sources surface rendering
    - labels
    - fine grid
    - projections
    - virtual screen
    New features to be added soon
    - surface textures with histograms
    - embedded OE editor
    - interactive re-tracing in the background process

--- xrt/gui/commons/gl.py
+++ xrt/gui/commons/gl.py
@@ -16,11 +16,15 @@ if isOpenGL:
         glDisableClientState, glRasterPos3f, glPushMatrix, glTranslatef,\
         glScalef, glPopMatrix, glFlush, glVertexPointerf, glColorPointerf,\
         glLineWidth, glDrawArrays, glMap1f, glMapGrid1f, glEvalMesh1,\
-        glMap2f, glMapGrid2f, glEvalMesh2,\
+        glMap2f, glMapGrid2f, glEvalMesh2, glFinish, glGetFloatv,\
+        glGenBuffers, glBindBuffer, glGetAttribLocation,\
+        glEnableVertexAttribArray, glVertexAttribPointer, glBufferData,\
         glLightModeli, glLightfv, glGetIntegerv, glColor4f, glVertex3f,\
         glBegin, glEnd, glViewport, glMaterialf, glHint, glPointSize,\
+        glAlphaFunc, glDrawElements, glStencilOp, glStencilFunc, glDepthMask,\
+        glReadPixelsui, glReadPixels, glBindFramebuffer, glDepthFunc,\
         GL_FRONT_AND_BACK, GL_AMBIENT, GL_DIFFUSE, GL_SPECULAR, GL_EMISSION,\
-        GL_FRONT, GL_SHININESS, GL_PROJECTION, GL_MODELVIEW,\
+        GL_FRONT, GL_SHININESS, GL_PROJECTION, GL_MODELVIEW, GL_INT,\
         GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT, GL_MULTISAMPLE, GL_BLEND,\
         GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_POINT_SMOOTH, GL_COLOR_ARRAY,\
         GL_LINE, GL_LINE_SMOOTH, GL_LINE_SMOOTH_HINT, GL_NICEST,\
@@ -30,16 +34,23 @@ if isOpenGL:
         GL_LIGHT_MODEL_TWO_SIDE, GL_LIGHT0, GL_POSITION, GL_SPOT_DIRECTION,\
         GL_SPOT_CUTOFF, GL_SPOT_EXPONENT, GL_TRIANGLE_FAN, GL_VIEWPORT,\
         GL_LINES, GL_MODELVIEW_MATRIX, GL_PROJECTION_MATRIX, GL_LINE_WIDTH,\
-        GL_LINE_STRIP
+        GL_LINE_STRIP, GL_FLOAT, GL_ARRAY_BUFFER, GL_DYNAMIC_DRAW,\
+        GL_PROGRAM_POINT_SIZE, GL_TRIANGLES, GL_TEXTURE_3D, GL_ALPHA_TEST,\
+        GL_GREATER, GL_UNSIGNED_SHORT, GL_CONSTANT_ALPHA, GL_SRC_COLOR,\
+        GL_STENCIL_BUFFER_BIT, GL_STENCIL_TEST, GL_KEEP, GL_REPLACE, GL_ALWAYS,\
+        GL_UNSIGNED_INT, GL_STENCIL_INDEX, GL_RGBA, GL_DEPTH_COMPONENT,\
+        GL_READ_FRAMEBUFFER, GL_DRAW_FRAMEBUFFER, GL_NEAREST, GL_POLYGON_SMOOTH,\
+        GL_FALSE, GL_LESS, GL_TRUE
 
     from OpenGL.GLU import gluPerspective, gluLookAt, gluProject
 
-    from OpenGL.GLUT import glutBitmapCharacter, glutStrokeCharacter,\
-        glutInit, glutInitDisplayMode, glutStrokeWidth,\
-        GLUT_BITMAP_HELVETICA_10,\
-        GLUT_BITMAP_HELVETICA_12, GLUT_BITMAP_HELVETICA_18,\
-        GLUT_BITMAP_TIMES_ROMAN_10, GLUT_BITMAP_TIMES_ROMAN_24,\
-        GLUT_STROKE_ROMAN, GLUT_RGBA, GLUT_DOUBLE, GLUT_DEPTH,\
-        GLUT_STROKE_MONO_ROMAN
+#    from OpenGL.GLUT import glutBitmapCharacter, glutStrokeCharacter,\
+#        glutInit, glutInitDisplayMode, glutStrokeWidth,\
+#        GLUT_BITMAP_HELVETICA_10,\
+#        GLUT_BITMAP_HELVETICA_12, GLUT_BITMAP_HELVETICA_18,\
+#        GLUT_BITMAP_TIMES_ROMAN_10, GLUT_BITMAP_TIMES_ROMAN_24,\
+#        GLUT_STROKE_ROMAN, GLUT_RGBA, GLUT_DOUBLE, GLUT_DEPTH,\
+#        GLUT_STROKE_MONO_ROMAN, GLUT_STENCIL
+#
+#    from OpenGL.arrays import vbo
 
-    from OpenGL.arrays import vbo
