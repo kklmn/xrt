ca8c221f9125025893c29e0e45968fb457f4f025
Roman Chernikov <yxrmz@users.noreply.github.com>
2025-03-28 11:15:55 -0400

    Fix materialsDict generation

--- xrt/backends/raycing/__init__.py
+++ xrt/backends/raycing/__init__.py
@@ -1660,7 +1660,7 @@ class BeamLine(object):
                                                     rmats.Multilayer)):
                             materialsDict[objInstance.uuid] = objInstance
                     self.materialsDict.update(materialsDict)
-                    _ = self.export_to_json()  # layoutStr is populated inside
+                _ = self.export_to_json()  # layoutStr is populated inside
 
                 self.blViewer = xrtglow.xrtGlow(layout=self.layoutStr,
                                                 **kwargs)
