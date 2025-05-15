bbdd2ae762a9e92915ec943765a55a7464d0dcaf
Roman Chernikov <yxrmz@users.noreply.github.com>
2025-05-15 16:13:15 -0400

    Initial integration with Qook
    Works with previously saved templates.
    TODO:
     - Dynamic flow
     - Adding/removing/moving elements
     - Renaming elements
     - Materials

--- xrt/backends/raycing/oes.py
+++ xrt/backends/raycing/oes.py
@@ -2629,8 +2629,8 @@ class ParaboloidFlatLens(Plate):
                     returnLocalAbsorbed=returnLocalAbsorbed)
 #            self.bl.flowSource = tmpFlowSource
         else:
-            tmpFlowSource = self.bl.flowSource
-            self.bl.flowSource = 'multiple_refract'
+#            tmpFlowSource = self.bl.flowSource
+#            self.bl.flowSource = 'multiple_refract'
             tempCenter = [c for c in self.center]
             beamIn = beam
             step = 2.*self.zmax + self.t\
