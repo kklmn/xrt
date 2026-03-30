996afc8abb0ceca47bbe5a40fdded1ed6d86676c
Roman Chernikov <yxrmz@users.noreply.github.com>
2026-03-29 21:16:05 -0400

    Refactored scene saving/restoring

--- xrt/gui/xrtGlow/_constants.py
+++ xrt/gui/xrtGlow/_constants.py
@@ -4,10 +4,10 @@ Created on Tue Jan 27 15:56:58 2026
 
 """
 
+from ..commons import qt
+
 __author__ = "Roman Chernikov, Konstantin Klementiev"
 __date__ = "27 Jan 2026"
-
-from ..commons import qt  # analysis:ignore
 
 msg_start = {"command": "start"}
 msg_stop = {"command": "stop"}
@@ -44,3 +44,95 @@ scr_m = qt.QMatrix4x4(1, 0, 0, 0,  0, 0, -1, 0,  0, 1, 0, 0,  0, 0, 0, 1)
 
 _DEBUG_ = True  # If False, exceptions inside the module are ignored
 MAXRAYS = 500000
+
+DEFAULT_SCENE_SETTINGS = {
+    'aspect': 1,
+    'viewPortGL': [0, 0, 500, 500],
+    'cameraAngle': 60.,
+    'cameraDistance': 3.5,
+
+    'scaleVec': [1e3, 1e1, 1e3],
+    'rotations': [0., 0.],
+
+    'coordOffset': [0., 0., 0.],
+    'tmpOffset': [0., 0., 0.],
+    'tVec': [0., 0., 0.],
+    'mModLocal': qt.QMatrix4x4(),
+
+    'colorAxis': 'energy',
+    'colorMin': 1e20,
+    'colorMax': -1e20,
+    'selColorMin': 1e20,
+    'selColorMax': -1e20,
+    'cutoffI': 0.01,
+    'globalNorm': True,
+    'iHSV': False,
+    'lineOpacity': 0.1,
+    'lineWidth': 1,
+    'pointOpacity': 0.1,
+    'pointSize': 1,
+
+    'drawGrid': True,
+    'perspectiveEnabled': True,
+    # 'fineGridEnabled': False,
+    'aPos': [0.9, 0.9, 0.9],
+    'projectionsVisibility': [0, 0, 0],
+
+    'lineProjectionOpacity': 0.1,
+    'lineProjectionWidth': 1,
+    'pointProjectionOpacity': 0.1,
+    'pointProjectionSize': 1,
+
+    'enableAA': False,
+    'globalColors': True,
+    'linesDepthTest': False,
+    'pointsDepthTest': False,
+    'invertColors': False,
+    'autoSizeOe': True,
+    'showLostRays': False,
+    'showLocalAxes': False,
+    'showInternalBeam': True,
+
+    'oeThickness': 5.,
+    'oeThicknessForce': None,
+    'slitThicknessFraction': 50.,
+    'maxLen': 1.,
+
+    'fontSize': 5,
+    'labelCoordPrec': 1,
+    'tiles': [25, 25]
+    }
+
+COLOR_CONTROL_LABELS = {
+    'globalNorm': 'Global Normalization',
+    'iHSV': 'Intensity as HSV Value'
+    }
+
+SCENE_CONTROL_LABELS = {
+    # 'drawGrid': 'checkDrawGrid',
+    # 'perspectiveEnabled': 'checkPerspect',
+    'enableAA': 'Enable antialiasing',
+    'globalColors': 'Use global colors',
+    'linesDepthTest': 'Depth test for Lines',
+    'pointsDepthTest': 'Depth test for Points',
+    'invertColors': 'Invert scene color',
+    'autoSizeOe': 'OE size match beam',
+    'showLostRays': 'Show lost rays',
+    'showLocalAxes': 'Show local axes',
+    'showInternalBeam': 'Show internal beams in multi-surface OEs',
+    }
+
+SCENE_TEXTEDITS = {
+    'oeThickness': {
+            'label': 'Default OE thickness, mm',
+            'tooltip': 'For OEs that do not have thickness'},
+    'oeThicknessForce': {
+            'label': 'Force OE thickness, mm',
+            'tooltip': 'For OEs that have thickness, e.g. plates or lenses'},
+    'slitThicknessFraction': {
+            'label': 'Aperture frame size, %',
+            'tooltip': ''},
+    'maxLen': {
+            'label': 'Scene limit, mm',
+            'tooltip': ''},
+   }
