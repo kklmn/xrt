1e6955d1261af7de821a1fdaf2fe2721ab2e7d57
Roman Chernikov <rchernikov@gmail.com>
2026-08-19 14:39:04 -0400

    Expose geom sources totalFlux in Qook

--- xrt/backends/raycing/sources/beams.py
+++ xrt/backends/raycing/sources/beams.py
@@ -21,7 +21,7 @@ allArguments = ('bl', 'name', 'center', 'pitch', 'roll', 'yaw', 'nrays',
                 'polarization', 'filamentBeam',
                 'uniformRayDensity', 'nx', 'nz', 'withCentralRay',
                 'autoAppendToBL', 'customField', 'gp', 'gIntervals', 'nRK',
-                'targetOpenCL', 'precisionOpenCL', 'fileName')
+                'targetOpenCL', 'precisionOpenCL', 'fileName', 'totalFlux')
 
 
 class BeamProxy(object):
