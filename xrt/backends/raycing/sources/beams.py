99412a44ecd3dbdf491ce888fcd7040f68c89844
kklmn <konstantin.klementiev@gmail.com>
2026-09-23 11:46:09 +0200

    Update beams.py

--- xrt/backends/raycing/sources/beams.py
+++ xrt/backends/raycing/sources/beams.py
@@ -182,7 +182,7 @@ class Beam(object):
                 if withAmplitudes:
                     self.Es = np.zeros(nrays, dtype=complex)
                     self.Ep = np.zeros(nrays, dtype=complex)
-        if type(forceState) == int:  # no isinstance() here!!! None is int
+        if isinstance(forceState, int) and not isinstance(forceState, bool):
             self.state[:] = forceState
         if not hasattr(self, 'parentId'):
             self.parentId = None
